6f9a4ac73038a6ca37f2f9faec5f8856f0c1ee8f
inaguha <inaguha@users.noreply.github.com>
2020-05-14 20:42:14 +0530

    Code file created.

---
+++ Python-For-Data-Science/code.py
@@ -0,0 +1,31 @@
+# --------------
+class_1 = ['Geoffrey Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio']
+class_2 = ['Hilary Mason','Carla Gentry','Corinna Cortes']
+new_class = class_1 + class_2
+print(new_class)
+new_class.append('Peter Warden')
+print(new_class)
+new_class.remove('Carla Gentry')
+print(new_class)
+courses = {'Math': 65, 'English' : 70, 'History' : 80, 'French' : 70, 'Science' : 60}
+total = 0
+total = courses['Math'] + courses['English'] + courses['History'] + courses['French'] + courses['Science']
+percentage = total*100 / 500
+print(total)
+print(percentage)
+mathematics = {'Geoffrey Hinton': 78, 'Andrew Ng': 95, 'Sebastian Raschka': 65, 'Yoshua Benjio': 50, 'Hilary Mason':70, 'Corinna Cortes':66, 'Peter Warden':75}
+
+topper = max(mathematics, key = mathematics.get)
+print(topper)
+topper = 'andrew ng'
+split = topper.split()
+print(split)
+first_name = split[0]
+last_name = split[-1]
+print(first_name)
+print(last_name)
+full_name = last_name + ' ' + first_name
+certificate_name = full_name.upper()
+print(certificate_name)
+
+
